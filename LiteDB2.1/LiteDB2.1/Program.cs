8dcc7ad16ae74cc8486ea7f7c08aeca08154a80e
LeandroArias <133369712+LeandritoArias@users.noreply.github.com>
2023-07-15 17:07:06 -0400

    Le menti profe si me faltaba mucho :c

---
+++ LiteDB2.1/LiteDB2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteDB2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BsonMapper.Global.RegisterType<DateTimeOffset>
+                    (
+                        serialize: obj =>
+                        {
+                            var doc = new BsonDocument();
+                            doc["DateTime"] = obj.DateTime.Ticks;
+                            doc["Offset"] = obj.Offset.Ticks;
+                            return doc;
+                        },
+                        deserialize: doc => new DateTimeOffset(doc["DateTime"].AsInt64, new TimeSpan(doc["Offset"].AsInt64)));
+        }
+    }
+}
